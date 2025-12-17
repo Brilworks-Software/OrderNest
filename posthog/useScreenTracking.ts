d2800568e50197b64e2153e9f0269187b619b7f0
Bhavik <bhavik.b@brilworks.com>
2025-12-17 09:15:54 +0530

    fix post hog screen name and add AD_ID permission in the app.json

--- posthog/useScreenTracking.ts
+++ posthog/useScreenTracking.ts
@@ -1,13 +1,21 @@
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
-import { usePathname } from 'expo-router';
+import { usePathname, useSegments } from 'expo-router';
 import posthogWeb from 'posthog-js';
 import { usePostHog } from 'posthog-react-native';
+
+function getScreenName(pathname: string, segments: string[]) {
+  if (pathname === '/' || segments.length === 0) return 'Home';
+
+  return segments.filter((s) => !s.startsWith('(')).join('_');
+}
 
 export function useScreenTracking() {
   const pathname = usePathname();
   const posthogNative = Platform.OS !== 'web' ? usePostHog() : null;
-
+  const segments = useSegments();
+  const screenName = getScreenName(pathname, segments);
+  
   useEffect(() => {
     if (!pathname) return;
 
@@ -23,6 +31,7 @@ export function useScreenTracking() {
       posthogNative.capture('screen_view', {
         screen: pathname,
         platform: Platform.OS,
+        $screen_name: screenName,
       });
     }
   }, [pathname]);
